0697e4c7770f0103a1eda0481dcecf22c36a68f1
alexsjcho <alexsjcho@gmail.com>
2023-03-17 16:06:28 -0700

    added lots of video price estimation logic

---
+++ rtcEstimations/calculations/aggregateResolution.js
@@ -0,0 +1,45 @@
+import { RESOLUTION } from "../constant/constant";
+
+const totalResolutionForUser = (videoProfile, noOfHost, snLogic) => {
+  let aggregateHost = 0;
+  if (snLogic === true) {
+    aggregateHost = noOfHost - 1;
+  } else {
+    aggregateHost = noOfHost;
+  }
+  switch (videoProfile) {
+    case 120:
+      return 120 * 160 * aggregateHost;
+    case 180:
+      return 180 * 320 * aggregateHost;
+    case 240:
+      return 240 * 320 * aggregateHost;
+    case 360:
+      return 360 * 640 * aggregateHost;
+    case 480:
+      return 480 * 640 * aggregateHost;
+    case 720:
+      return 720 * 1280 * aggregateHost;
+    case 1080:
+      return 1080 * 1920 * aggregateHost;
+    default:
+      return 120 * 160;
+  }
+};
+
+const videoStreamingVariant = (value) => {
+  if (value <= 921600) {
+    return RESOLUTION.HD;
+  } else if (value > 921600 && value <= 2073600) {
+    return RESOLUTION.FULLHD;
+  } else if (value > 2073600 && value <= 3686400) {
+    return RESOLUTION.TWOK;
+  } else if (value > 3686400 && value <= 35251200) {
+    return RESOLUTION.TWOKPLUS;
+  }
+};
+export const aggregateVideo = (videoProfile, noOfHost, snLogic) => {
+  const value = totalResolutionForUser(videoProfile, noOfHost, snLogic);
+  const resolution = videoStreamingVariant(value);
+  return resolution;
+};
