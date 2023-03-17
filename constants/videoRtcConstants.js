d1487412b784789e67263fefaa86af5431c6fd4c
alexsjcho <alexsjcho@gmail.com>
2023-03-17 15:47:41 -0700

    starting on rtc-estimation features

---
+++ constants/videoRtcConstants.js
@@ -0,0 +1,20 @@
+export const PRODUCTTYPE = {
+  VIDEO: "video",
+  STREAMING: "streaming",
+  VOICE: "voice",
+};
+export const RESOLUTION = {
+  HD: "HD",
+  FULLHD: "FULL HD",
+  TWOK: "2K",
+  TWOKPLUS: "2K+",
+};
+export const AUDIENCETYPE = {
+  STANDARD: "standard",
+  PREMIUM: "premium",
+};
+
+export const STREAMTYPE = {
+  AUDIOONLY: "audio",
+  AUDIOANDVIDEO: "audioVideo",
+};
