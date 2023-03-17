ce68b351b16b1a9dde344ecd15a4bbdeea2df74c
alexsjcho <alexsjcho@gmail.com>
2023-03-17 16:21:43 -0700

    successfully used Github ClickUp integration to track GitHub/ClickUp Tasks simutaneoulsy. doing research on X State library to see if this library would be best solution to handle state management for this app

---
+++ rtcEstimations/constants/videoProfileConstants.js
@@ -0,0 +1,9 @@
+export const videoProfiles = [
+  { value: 120, label: "120p" },
+  { value: 180, label: "180p" },
+  { value: 240, label: "240p" },
+  { value: 360, label: "360p" },
+  { value: 480, label: "480p" },
+  { value: 720, label: "720p" },
+  { value: 1080, label: "1080p" },
+];
